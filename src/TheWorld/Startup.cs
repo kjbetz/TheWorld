617b2366794f81b862578168db06bd1364039dd1
kristopher.betz <kristopher.j.betz.mil@mail.mil>
2015-11-04 14:46:28 -0500

    Part of module 4, add MVC, and update beta7 to beta8

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -13,11 +13,21 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+             });
         }
     }
 }
