bc50bca7c9a1c6bc8163cbc623a753060497a799
kristopher.betz <kristopher.j.betz.mil@mail.mil>
2015-11-04 14:01:07 -0500

    Finished module 3.

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,12 +1,33 @@
-﻿var ele = document.getElementById("username");
-ele.innerHTML = "Kristopher Betz";
+﻿(function () {
+    //$("#username").text("Kristopher Betz");
 
-var main = document.getElementById("main");
+    //var main = document.getElementById("main");
+    //main = $("#main");
+    //main.on("mousenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
 
-main.onmouseenter = function () {
-    main.style = "background-color: #888;";
-};
+    //main.on("mouseleave", function () {
+    //    main.style.backgroundColor = "";
+    //});
 
-main.onmouseleave = function () {
-    main.style = "";
-};
+    //var menuItems = $("ul.menu li a");
+
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
