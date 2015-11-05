f92f7713b724808d05cb7152a1a58c9c81df2b7c
kristopher.betz <kristopher.j.betz.mil@mail.mil>
2015-11-05 15:56:24 -0500

    End of module 6, MVC. Added posting, IMailSender interface, depency injection, DebugMailSender, setup config system, server side validation.

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -3,11 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -20,6 +29,33 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem");
+                }
+
+                if (_mailService.SendMail(
+                    email,
+                    email,
+                    $"(Contact Page from {model.Name} ({model.Email})",
+                    model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent, thanks!";
+                }
+            }
+            
             return View();
         }
     }
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -5,15 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using TheWorld.Services;
+using Microsoft.Framework.Configuration;
+using Microsoft.Dnx.Runtime;
 
 namespace TheWorld
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddScoped<IMailService, DebugMailService>();
         }
 
         public void Configure(IApplicationBuilder app)
